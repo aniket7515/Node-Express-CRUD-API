5877ded9faa3485679e08f4ff0586eb226160497
Aniket <aniketdhokane2071@gmail.com>
2022-07-09 19:22:58 +0530

    Update

--- routes/users.js
+++ routes/users.js
@@ -70,7 +70,26 @@ router.delete('/:id',(req,res)=>{
 })
 
 
+// updateing the user information we will use Patch method
 
+router.patch('/:id',(req,res)=>{
+    const { id } = req.params;
+    // what  user can update
+    const {firstName, lastName,age } = req.body;
+    const user= users.find((user)=> user.id === id)
+
+    if(firstName){
+        user.firstName=firstName;
+    }
+    if(lastName){
+        user.lastName=lastName;
+    }
+    if(age){
+        user.age=age;
+    }
+
+    res.send(` user woth the id ${id} has been updated`)
+})
 
 
 export default router;
